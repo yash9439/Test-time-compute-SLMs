26ad4a0de699be2db5fc627247b6a4b7c41ca827
Your Name <yash9439@gmail.com>
2025-03-10 03:22:56 +0530

    Adding Adapter Weights for SFT Experiments

---
+++ Unsloth-SFT/bash_script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python unsloth_5Epoch_1e_4.py > 5Epoch_1e_4.txt 2>&1
+python unsloth_5Epoch_1e_6.py > 5Epoch_1e_6.txt 2>&1
---
+++ Unsloth-SFT/unsloth_5Epoch_1e_4.py
@@ -0,0 +1,111 @@
+import torchvision
+from unsloth import FastLanguageModel
+import torch
+import wandb
+import os
+
+os.environ["WANDB_API_KEY"] = "f0183b700c7547893e5388c76bf32e5e02af677a"
+os.environ["WANDB_PROJECT"] = "s1_unsloth_exp1"
+os.environ["WANDB_ENTITY"] = "yash9439"
+os.environ["WANDB_DIR"] = "./wandb_logs"
+
+wandb.login(key=os.environ["WANDB_API_KEY"]) 
+
+run = "5Epoch_1e_4"
+
+max_seq_length = 32768 # Choose any! We auto support RoPE Scaling internally!
+dtype = None          # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+load_in_4bit = True   # Use 4bit quantization to reduce memory usage. Can be False.
+
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = "unsloth/Qwen2.5-1.5B-Instruct",
+    max_seq_length = max_seq_length,
+    dtype = dtype,
+    load_in_4bit = load_in_4bit,
+)
+
+model = FastLanguageModel.get_peft_model(
+    model,
+    r = 16, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+    target_modules = ["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj",],
+    lora_alpha = 16,
+    lora_dropout = 0,    # Supports any, but = 0 is optimized
+    bias = "none",       # Supports any, but = "none" is optimized
+    # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+    use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
+    random_state = 3407,
+    use_rslora = False,  # We support rank stabilized LoRA
+    loftq_config = None, # And LoftQ
+)
+
+from datasets import load_dataset
+from unsloth.chat_templates import get_chat_template
+from unsloth.chat_templates import standardize_sharegpt
+
+tokenizer = get_chat_template(
+    tokenizer,
+    chat_template = "qwen-2.5",
+)
+
+def formatting_prompts_func(examples):
+    convos = examples["text"]
+    texts = [tokenizer.apply_chat_template(convo, tokenize=False, add_generation_prompt=False) for convo in convos]
+    return { "text" : texts, }
+
+dataset = load_dataset("simplescaling/s1K_tokenized", split="train")
+# dataset = standardize_sharegpt(dataset)
+# dataset = dataset.map(formatting_prompts_func, batched=True)
+
+from trl import SFTTrainer
+from transformers import TrainingArguments, DataCollatorForSeq2Seq
+from unsloth import is_bfloat16_supported
+
+trainer = SFTTrainer(
+    model = model,
+    tokenizer = tokenizer,
+    train_dataset = dataset,
+    dataset_text_field = "text",
+    max_seq_length = max_seq_length,
+    data_collator = DataCollatorForSeq2Seq(tokenizer = tokenizer),
+    dataset_num_proc = 4,
+    packing = False, # Can make training 5x faster for short sequences.
+    args = TrainingArguments(
+        per_device_train_batch_size = 16,
+        gradient_accumulation_steps = 1, # Fixed major bug in latest Unsloth
+        warmup_steps = 5,
+        num_train_epochs = 5, # Set this for 1 full training run.
+        # max_steps = 30,
+        learning_rate = 1e-4,
+        fp16 = not is_bfloat16_supported(),
+        bf16 = is_bfloat16_supported(),
+        logging_steps = 1,
+        optim = "paged_adamw_8bit", # Save more memory
+        weight_decay = 0.01,
+        lr_scheduler_type = "linear",
+        seed = 3407,
+        output_dir = f"outputs_{run}",
+        report_to = "wandb", # Use this for WandB etc
+        run_name = run
+    ),
+)
+
+from unsloth.chat_templates import train_on_responses_only
+
+trainer = train_on_responses_only(
+    trainer,
+    instruction_part = "<|im_start|>user\n",
+    response_part = "<|im_start|>assistant\n",
+)
+
+#@title Show current memory stats
+gpu_stats = torch.cuda.get_device_properties(0)
+start_gpu_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
+
+print(f"GPU = {gpu_stats.name}. Max memory = {max_memory} GB.")
+print(f"{start_gpu_memory} GB of memory reserved.")
+
+trainer_stats = trainer.train()
+
+model.save_pretrained(run) # Local saving
+tokenizer.save_pretrained(run)
